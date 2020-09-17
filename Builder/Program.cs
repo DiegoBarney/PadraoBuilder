e28566f1e8afc6bd1e87523b366a0d9cf44cbc89
DiegoBarney <komyhu@hotmail.com>
2020-09-16 21:43:50 -0300

    Primeiro Commit do Projeto Builder
    Commit 16/09/2020

---
+++ Builder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NotaFiscal notafiscal = new NotaFiscalBuilder().paraEmpresa("Diego Pimentel")
+                  .comCnpj("XXX.XXX.XXX/0001-XX")
+                  .comItemAdd(new ItemDaNota("Teclado", 45.0))
+                  .comItemAdd(new ItemDaNota("Mouse", 12.0))
+                  .comItemAdd(new ItemDaNota("Monitor", 300.0))
+                  .comObservacao("Entregar nota fiscal em maos")
+                  .construir();
+
+
+            Console.WriteLine("Razao Social: " + notafiscal.getRazaoSocial() + "CNPJ: "+notafiscal.getCnpj() + "Valor: " + notafiscal.getValorBruto());
+            Console.ReadLine();
+        }
+    }
+}
